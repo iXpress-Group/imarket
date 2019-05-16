ca57bd6cb784767a0b943241547690712d1304be
Engineersticity <ekirui17@alustudent.com>
2019-05-16 17:58:17 +0300

    Made the homepage a little fancy

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,10 +13,14 @@ class Home extends Component {
             <div className="Home">
                 <Header/>
                 <div className="home-container">
+                    <img className="home-img" src={require('../assets/images/coverpage.png')} alt=""/>
+
                     <p>This is the homepage. Click <Link className='home-link' to={'dev/products'}>here</Link> to go to
                         the products page</p>
                     <p>This is the homepage. Click <Link className='home-link' to={'dev/refer_a_friend'}>here</Link> to go to
                         the refer a friend page</p>
+
+
                 </div>
                 <Footer/>
             </div>
